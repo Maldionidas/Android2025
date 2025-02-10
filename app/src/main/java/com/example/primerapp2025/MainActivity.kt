64542a0f7be44b63afaa6c5f4c62db184fbd8794
Maldonado <maldonadoggmx@gmail.com>
2025-02-10 13:36:35 -0600

    clase 10 feb 2025

--- app/src/main/java/com/example/primerapp2025/MainActivity.kt
+++ app/src/main/java/com/example/primerapp2025/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
@@ -11,7 +13,9 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.primerapp2025.ui.theme.PrimerApp2025Theme
 
 class MainActivity : ComponentActivity() {
@@ -20,34 +24,25 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PrimerApp2025Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                GreetingPreview()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-    //Button {
-    //        onClick =  println("Hola mundo")
-    //}
-
-}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    PrimerApp2025Theme {
-        Greeting("Android")
-
+    Content(mensaje1= "Hola", mensaje2= "amigo")
+}
+@Composable
+fun Content(mensaje1: String,mensaje2: String){
+   Column {
+        Text(mensaje1, fontSize = 30.sp)
+        Text(mensaje2, lineHeight = 30.sp)
+        Text("Ivan", fontWeight = FontWeight.Bold,
+            fontSize = 30.sp,
+            lineHeight = 32.sp)
     }
 }
