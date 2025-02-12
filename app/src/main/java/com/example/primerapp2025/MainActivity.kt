afc217f2d4fbd7bf4580463ae0ce7e664050cead
Maldonado <maldonadoggmx@gmail.com>
2025-02-12 13:50:19 -0600

    practica botones 12 febrero parte 1

--- app/src/main/java/com/example/primerapp2025/MainActivity.kt
+++ app/src/main/java/com/example/primerapp2025/MainActivity.kt
@@ -4,14 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.primerapp2025.ui.theme.PrimerApp2025Theme
 
 class MainActivity : ComponentActivity() {
@@ -20,34 +28,60 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PrimerApp2025Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    setContent()
                 }
             }
         }
+    
+}
+@Preview(showBackground = true)
+@Composable
+fun setContent() {
+    Column {
+        SimpleButton()
+        ButtonWithColor()
+        ButtonWithTwoTextView()
+        ButtonWithIcon()
+    }
+
+}
+@Composable
+fun SimpleButton() {
+    Button(onClick = {
+        //your onclick code here
+    }) {
+        Text(text = "Simple Button")
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-    //Button {
-    //        onClick =  println("Hola mundo")
-    //}
+fun ButtonWithColor(){
+    Button(onClick = {
+        //your onclick code
+    },
+        colors = ButtonDefaults.buttonColors(Color.DarkGray))
 
+    {
+        Text(text = "Button with gray background",color = Color.White)
+    }
 }
-
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    PrimerApp2025Theme {
-        Greeting("Android")
+fun ButtonWithTwoTextView() {
+    Button(onClick = {
+        //your onclick code here
+    }) {
+        Text(text = "Click ", color = Color.Magenta)
+        Text(text = "Here", color = Color.Green)
+    }
+}
+@Composable
+fun ButtonWithIcon() {
+    Button(onClick = {}) {
+        Image(
+            painterResource(id = R.drawable.carrito),
+            contentDescription ="Cart button icon",
+            modifier = Modifier.size(20.dp))
 
+        Text(text = "Add to cart",Modifier.padding(start = 10.dp))
     }
 }
