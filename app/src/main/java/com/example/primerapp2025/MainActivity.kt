b2f8825e1716c3cd14fe30ceacc3015bfd556ef6
Maldonado <maldonadoggmx@gmail.com>
2025-02-17 13:49:13 -0600

    practica imagenes

--- app/src/main/java/com/example/primerapp2025/MainActivity.kt
+++ app/src/main/java/com/example/primerapp2025/MainActivity.kt
@@ -4,14 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+//import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+//import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
+//import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.primerapp2025.ui.theme.PrimerApp2025Theme
 
 class MainActivity : ComponentActivity() {
@@ -20,34 +32,66 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PrimerApp2025Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                GreetingPreview()
+
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-    //Button {
-    //        onClick =  println("Hola mundo")
-    //}
 
-}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
+    Column /*modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center*/{
     PrimerApp2025Theme {
-        Greeting("Android")
-
+        SimpleImage()
+        CircleImageView()
+        RoundCornerImageView()
+        ImageWithBackgroundColor()
     }
+}
+}
+@Composable
+fun SimpleImage() {
+    Image(
+        painter = painterResource(id = R.drawable.pelon),
+        contentDescription = "Andy Rubin",
+        modifier = Modifier.fillMaxWidth()
+    )
+}
+@Composable fun CircleImageView() {
+    Image( painter = painterResource(R.drawable.pelon),
+        contentDescription = "Circle Image",
+        contentScale = ContentScale.Crop,
+        modifier = Modifier
+            .size(128.dp)
+            .clip(CircleShape)
+            .border(5.dp, Color.Gray, CircleShape)
+    )
+}
+@Composable
+fun RoundCornerImageView() {
+    Image(
+        painter = painterResource(R.drawable.pelon),
+        contentDescription = "Round corner image",
+        contentScale = ContentScale.Crop,
+        modifier = Modifier
+            .size(128.dp)
+            .clip(RoundedCornerShape(10))
+            .border(5.dp, Color.Gray, RoundedCornerShape(10))
+    )
+}
+@Composable
+fun ImageWithBackgroundColor() {
+    Image(
+        painter = painterResource(id = R.drawable.cart_24),
+        contentDescription = "",
+        modifier = Modifier
+            .size( 200.dp)
+            .background(Color.DarkGray)
+            .padding(20.dp)
+    )
 }
